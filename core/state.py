448d36fc3c188da8a10cbc8d5b3065636af36285
Zhalslar <zsl2936169201@gmail.com>
2025-12-30 10:28:17 +0800

    Update state.py

--- core/state.py
+++ core/state.py
@@ -9,7 +9,7 @@ class GroupState(BaseModel):
     bot_msgs: deque = Field(default_factory=lambda: deque(maxlen=5))
     """Bot消息缓存"""
     msg_queue: deque[str] = Field(default_factory=lambda: deque(maxlen=10))
-    """被顶了多少条消息"""
+    """用户消息缓存"""
     name_to_qq: OrderedDict[str, str] = Field(default_factory=lambda: OrderedDict())
     """昵称 -> QQ"""
 
